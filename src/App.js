d7cb2cbc210694dfd5d88c004c3a033ed679ecdf
Colin Whitney <colin.whitney@gmail.com>
2020-11-26 13:20:53 -0500

    added react-router-dom and react-bootstrap-CW

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button'
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>The Sooshi App</h1>
+        <Button>Click Me</Button>
       </div>
     )
   }
